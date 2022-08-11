d7e5bae407dbef694e5b561202bbf88db2c9797a
SALT0601 <thfxm0601@gmail.com>
2022-08-11 22:59:18 +0900

    Fix:해설 코드 추가
    해설 코드 추가

--- Algo/src/inflearn/No_1_11문자열압축.java
+++ Algo/src/inflearn/No_1_11문자열압축.java
@@ -29,6 +29,19 @@ public class No_1_11문자열압축 {
 			}
 			ans = ans.replace("1", "");		
 			return ans;
+			
+			//해설 코드
+			// String ans="";
+			//int s=0, e=0, cnt=1;
+			//str = str + " ";//빈문자로 패딩
+			//for(int i=0; i<s.length()-1; i++){
+			//if(s.charAt(i) == s.charAt(i+1) cnt__;
+			//else{
+			//	ans += s.charAt(i)
+			//	if(cnt>1) ans += String.valueOf(cnt);//애초에 여기서부터 1 안 넣으면 replace 안해도 될듯
+			//	cnt=1;
+			//}
+			// }
 	
 	}
 	public static void main(String[] args) throws IOException {
--- Algo/src/inflearn/No_1_4단어뒤집기.java
+++ Algo/src/inflearn/No_1_4단어뒤집기.java
@@ -1,8 +1,6 @@
 package inflearn;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 
 public class No_1_4단어뒤집기 {
 	public static String solution(String str) {
